58b0481483ab18da040be396503a13afd6a20811
Alexei KLENIN <alexei.klenin@gmail.com>
2025-04-27 15:54:59 +0200

    advance

--- automation/patch.sh
+++ automation/patch.sh
@@ -2,6 +2,10 @@
 
 echo "Bumping version..."
 lerna version patch --yes --no-push --no-git-tag-version
-ROOT_VERSION=$(node -p "require('./lerna.json').version")
-jq ".version=\"$ROOT_VERSION\"" package.json > package.json.tmp && mv package.json.tmp package.json
+PROJECT_VERSION=$(node -p "require('./lerna.json').version")
+jq ".version=\"$PROJECT_VERSION\"" package.json > package.json.tmp && mv package.json.tmp package.json
 npm install
+
+echo "Commit changes..."
+git commit -am "HOTFIX ${PROJECT_VERSION}"
+git tag -a "v${PROJECT_VERSION}" -m "HOTFIX ${PROJECT_VERSION}"
