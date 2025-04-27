3e3d5ff8b4e1aa3c31f9e81c0d311774d451c1a9
Alexei KLENIN <alexei.klenin@gmail.com>
2025-04-27 15:47:14 +0200

    progress

--- automation/patch.sh
+++ automation/patch.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Bumping version"
+echo "Bumping version..."
 lerna version patch --yes --no-push --no-git-tag-version
+ROOT_VERSION=$(node -p "require('./lerna.json').version")
+jq ".version=\"$ROOT_VERSION\"" package.json > package.json.tmp && mv package.json.tmp package.json
+npm install
